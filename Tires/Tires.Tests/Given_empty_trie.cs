d0f17fb2158f2dcabeaa16b17c6bdf7ba01903cc
Norbert Raus <norbert.raus@gmail.com>
2012-12-09 18:36:41 +0000

    Adding Tries project - inital trie implementation

---
+++ Tires/Tires.Tests/Given_empty_trie.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Tries.Tests
+{
+    public class Given_empty_trie
+    {
+        private Trie<int> _trie;
+
+        //It was the best of times, it was the worst of times,
+
+        [SetUp]
+        public void SetUp()
+        {
+            _trie = new Trie<int>();
+        }
+
+        [Test]
+        public void should_be_empty()
+        {
+            Assert.That(_trie.Size, Is.EqualTo(0));
+            Assert.That(_trie.Keys.Count(), Is.EqualTo(0));
+        }
+                
+        [Test]
+        public void should_add_new_key()
+        {
+            _trie.Put("it", 1);
+            
+            Assert.That(_trie.Size, Is.EqualTo(1));
+            Assert.That(_trie.Keys.First(), Is.EqualTo("it"));
+        }
+
+        [Test]
+        public void should_add_two_keys()
+        {
+            _trie.Put("it", 1);
+            _trie.Put("was", 2);
+
+            Assert.That(_trie.Size, Is.EqualTo(2));
+            Assert.That(_trie.Keys.First(), Is.EqualTo("it"));
+            Assert.That(_trie.Keys.Skip(1).First(), Is.EqualTo("was"));
+        }
+
+        [Test]
+        public void should_get_value_for_key()
+        {
+            _trie.Put("it", 3);
+
+            Assert.That(_trie.GetValue("it"), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void should_override_key_value()
+        {
+            _trie.Put("it", 1);
+            _trie.Put("it", 2);
+
+            Assert.That(_trie.GetValue("it"), Is.EqualTo(2));
+        }
+    }
+
+   
+}
---
+++ Tires/Tires/Trie.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tries
+{
+    public class Trie<T>
+    {
+        public const int AlphabetSize = 256;
+        
+        private readonly Node _root = new Node(); //root node of the trie
+        private int _size = 0;
+
+        class Node
+        {
+            private Node[] _links = new Node[AlphabetSize];
+
+            public T Value { get; set; }
+            public Node[] Links { get { return _links; } }
+        }
+
+        public T GetValue(string key)
+        {
+            Node match = Get(_root, key);
+            if (match != null)
+            {
+                return match.Value;
+            }
+            return default(T);
+        }
+
+        public void Put(string key, T value)
+        {
+            Put(_root, key, value);
+        }
+
+        /// <summary>
+        /// Provides the number of keys in <see cref="Trie{T}"/>
+        /// </summary>
+        public int Size
+        {
+            get { return _size; }
+        }
+
+        /// <summary>
+        /// Provides all keys available in <see cref="Trie{T}"/>
+        /// </summary>
+        public IEnumerable<string> Keys
+        {
+            get { return KeysWithPrefix(string.Empty); }
+        }
+
+        private IEnumerable<string> KeysWithPrefix(string prefix)
+        {
+            return Collect(_root, prefix);
+        }
+
+        private IEnumerable<string> Collect(Node root, string prefix)
+        {
+            if (root == null) yield break;
+            if (!Equals(root.Value, default(T)))
+                yield return prefix;
+
+            for (int next = 0; next < AlphabetSize; next++)
+            {
+                foreach (var key in Collect(root.Links[next], prefix + (char)next))
+                {
+                    yield return key;
+                }
+            }
+
+        }
+
+        private Node Put(Node node, string key, T value, int position = 0)
+        {
+            if (node == null) node = new Node();
+            if (key.Length == position)
+            {
+                node.Value = value;
+                _size++;
+                return node;
+            }
+            var @char = key[position];
+            node.Links[@char] = Put(node.Links[@char], key, value, position + 1);
+            return node;
+        }
+        
+        private Node Get(Node node, string key, int position = 0)
+        {
+            if (node == null) return null;
+            if (key.Length == position) return node;
+
+            //get character at position
+            var @char = key[position];
+            return Get(node.Links[@char], key, position + 1);
+        }
+    }
+}
