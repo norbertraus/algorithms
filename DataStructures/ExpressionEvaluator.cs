93eab57fd24947a1410cce3d1cc0c0b3f28f9761
Norbert Raus <norbert.raus@gmail.com>
2013-04-06 19:46:00 +0100

    Stack and basic evaluator

---
+++ DataStructures/ExpressionEvaluator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+
+namespace DataStructures
+{
+    /// <summary>
+    /// Represents expression evaluator that uses E.W.Dijkstra algorithm with two stacks
+    /// </summary>
+    public class ExpressionEvaluator
+    {
+        /// <summary>
+        /// For simplicity we assume that all operands are separated by whitespace
+        /// </summary>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public double Evaluate(string expression)
+        {
+            var operators = new Stack_LinkedList<string>();
+            var values = new Stack_LinkedList<double>();
+
+            foreach(var term in expression.Split(' '))
+            {
+                if(term == "(") ; //ignore
+                else if(term == "+" || term == "-" || term == "*" || term == "/" || term == "sqrt") operators.Push(term);
+                else if(term == ")")
+                {
+                    var op = operators.Pop();
+                    var value = values.Pop(); //take one value from the top of the stack since there might be only single argument operator
+
+                    if(op == "+") value = values.Pop() + value;
+                    else if(op == "-") value = values.Pop() - value;
+                    else if(op == "*") value = values.Pop() * value;
+                    else if(op == "/") value = values.Pop() / value;
+                    else if(op == "sqrt") value = Math.Sqrt(value);
+
+                    //push calculated value on the stack
+                    values.Push(value);
+
+                }
+                else values.Push(double.Parse(term));
+            }
+            return values.Pop();
+        }
+
+        //TODO : Use operator precendence and parser for the input string
+
+        [TestFixture]
+        public class Tests
+        {
+            private ExpressionEvaluator _evaluator = new ExpressionEvaluator();
+
+            [Test]
+            public void should_calculate_simple_expression()
+            {
+                var value = _evaluator.Evaluate("( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )");
+
+                Assert.That(value, Is.EqualTo(101.0));
+            }
+
+            [Test]
+            public void should_calculate_with_sqrt()
+            {
+                var value = _evaluator.Evaluate("( ( 1 + sqrt ( 5.0 ) ) / 2.0 )");
+
+                Assert.That(Math.Round(value, 4), Is.EqualTo(1.6180));
+            }
+        }
+    }
+
+    
+}
---
+++ DataStructures/Stacks.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    public interface IStack<T> : IEnumerable<T>
+    {
+        void Push(T data);
+        T Pop();
+        int Size { get; }
+    }
+
+    /// <summary>
+    /// Represents simple push down stack implementation using linked lists
+    /// </summary>
+    /// <remarks>
+    /// Performance characteristics: Pop - O(1), Push - O(1)
+    /// </remarks>
+    public class Stack_LinkedList<T> : IStack<T>
+    {
+        /// <summary>
+        /// Represents linked list node for keeping the single value and pointer to the next node
+        /// </summary>
+        private class Node
+        {
+            public Node(T data)
+            {
+                Data = data;
+            }
+
+            public T Data { get; private set; }
+            public Node Next { get; set; }
+        }
+
+        private Node _first;
+        private int _size;
+
+        public void Push(T data)
+        {
+            var first = _first;
+            _first = new Node(data);
+            _first.Next = first;
+            _size--;
+        }
+
+        public T Pop()
+        {
+            if (_first != null)
+            {
+                var data = _first.Data;
+                _first = _first.Next;
+                _size++;
+
+                return data;
+            }
+            return default(T);
+        }
+
+        public int Size { get { return _size; } }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (Node node = _first; node != null; node = node.Next)
+            {
+                yield return node.Data;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+    
+    /// <summary>
+    /// Stack_LinkedList implementation using array. Pop - O(1), Push can be O(n)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Stack_Array<T> : IStack<T>
+    {
+        private T[] _items;
+        private int _size;
+
+        public Stack_Array()
+        {
+            _items = new T[4];
+        }
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Push(T data)
+        {
+            if (_size == _items.Length)
+            {
+                var newItems = new T[_items.Length * 2];
+                Array.Copy(_items, newItems, _items.Length);
+                
+                _items = newItems;
+            }
+
+            _items[_size++] = data;
+
+        }
+
+        /// <summary>
+        /// Performs 
+        /// </summary>
+        /// <returns></returns>
+        public T Pop()
+        {
+            if (_size > 0)
+            {
+                var data = _items[--_size];
+                _items[_size] = default(T);
+
+                //the items array is one-fourth full, hence we will shrink it by half
+                if (_size > 0 && _size == _items.Length/4)
+                {
+                    var newItems = new T[_items.Length / 2];
+                    Array.Copy(_items, newItems, _size);
+
+                    _items = newItems;
+                }
+                return data;
+            }
+            return default(T);
+        }
+
+        public int Size { get { return _size; } }
+    }
+}
